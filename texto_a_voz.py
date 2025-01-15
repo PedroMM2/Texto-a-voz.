9e3a1969c97e9f7e962196fc32d699b6c32a2113
PedroMM2 <pedromoreno2x@gmail.com>
2025-01-15 10:18:29 +0100

    Añadidos comentarios mas explícitos y se renombra la función principal.

--- texto_a_voz.py
+++ texto_a_voz.py
@@ -1,17 +1,20 @@
-import nltk
-from newspaper import Article
-from gtts import gTTS
+import nltk #nltk es el Kit de Herramientas de Lenguaje Natural.
+from newspaper import Article #newspaper es biblioteca que obtiene y analiza artículos de noticias.
+from gtts import gTTS #gTTS: biblioteca de Google Text-to-Speech para convertir texto a voz.
 
 # Descargar los datos necesarios para nltk
-nltk.download('punkt')
-nltk.download('punkt_tab')
+'''nltk. punkt se usa para la tokenización de oraciones, y punkt_tab se utiliza para 
+ciertas configuraciones de tokenización.'''
+nltk.download('punkt') # tokenización de oraciones
+nltk.download('punkt_tab')# configuraciones de tokenización.
 
-def convert_article_to_audio(url, output_file):
+def convert_to_audio(url, output_file):
     # Obtener el artículo de la URL
     article = Article(url)
-    article.download()
-    article.parse()
-    article.nlp()
+    article.download()#guarda el HTML de la página en atributo del objeto Article.
+    article.parse()#analiza el contenido HTML descargado y extrae el texto del artículo.
+    article.nlp() #nlp() aplica técnicas de Procesamiento de Lenguaje Natural 
+    # al artículo, como la obtención de resumen y palabras clave.
 
     # Obtener el texto del artículo
     text = article.text
@@ -21,8 +24,9 @@ def convert_article_to_audio(url, output_file):
 
     # Convertir el texto a voz
     try:
+    #En esta parte, utilizamos gTTS para convertir el texto del artículo a voz.
         tts = gTTS(text=text, lang='es')
-        tts.save(output_file)
+        tts.save(output_file)# Guardamos el archivo en output_file.
         print(f"El archivo de audio ha sido guardado como {output_file}")
     except Exception as e:
         print(f"Ocurrió un error al guardar el archivo de audio: {e}")
@@ -30,4 +34,4 @@ def convert_article_to_audio(url, output_file):
 if __name__ == "__main__":
     url = input("Introduce la URL del artículo: ")
     output_file = "articulo.mp3"
-    convert_article_to_audio(url, output_file)
+    convert_to_audio(url, output_file)
